85ec9acdc9df5889af224463651bd2441b210264
sofiabukovskaya <sofi454545@gmail.com>
2020-12-23 18:09:17 +0200

    Final commit

--- app/src/main/java/com/example/alzheimermobile/DoctorProfileActivity.kt
+++ app/src/main/java/com/example/alzheimermobile/DoctorProfileActivity.kt
@@ -10,18 +10,35 @@ import com.google.firebase.database.*
 class DoctorProfileActivity : AppCompatActivity() {
 //    private lateinit var database: FirebaseDatabase
 //    private lateinit var reference: DatabaseReference
+//
 //    private var tvFirstName: TextView? = null
 //    private var tvLastName: TextView? = null
+//    private var tvLastemail: TextView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_doctor_profile)
 //        database = FirebaseDatabase.getInstance()
 //        reference = database.getReference("users")
-//        tvFirstName = findViewById<TextView>(R.id.PatientName) as TextView
+//
+//        tvFirstName = findViewById<TextView>(R.id.textviewpationtname) as TextView
+//        tvLastName = findViewById<TextView>(R.id.textviewpationtsurname) as TextView
+//        tvLastemail = findViewById<TextView>(R.id.textviewpationtemail) as TextView
+
 
 
     }
+//    override fun onStart() {
+//        super.onStart()
+//        reference.addValueEventListener(object : ValueEventListener {
+//            override fun onDataChange(snapshot: DataSnapshot) {
+//                tvFirstName!!.text = snapshot.child("name").value as String
+//                tvLastName!!.text = snapshot.child("surname").value as String
+//                tvLastemail!!.text = snapshot.child("email").value as String
+//            }
+//            override fun onCancelled(databaseError: DatabaseError) {}
+//        })
+//    }
 
 
 }
